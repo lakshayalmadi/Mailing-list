27f337a66717cc5106361f8a308e614274dcfb1f
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-18 00:22:13 +0530

    final

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ app.post("/", function(req,res){
     }
 
     const request = https.request(url, options, function(response){
+        
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -44,7 +52,9 @@ app.post("/", function(req,res){
     request.end();
 });
 
-
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server is running on 3000");
