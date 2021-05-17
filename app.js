94b24e34a6d0c18c2b6bda61f1b1735a30981c6b
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-17 22:11:42 +0530

    added-mailchimp

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser  = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
@@ -12,20 +12,45 @@ app.get("/", function(req,res){
 });
 
 app.post("/", function(req,res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.eMail;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.eMail;
 
     var data = {
-        
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+    var jsonData = JSON.stringify(data);
+    const url = "https://us1.api.mailchimp.com/3.0/lists/defd742678";
+    const options = {
+        method: "POST",
+        auth: "lakshay:b0c876a1acad6d50b43a773159f4133e-us1"
     }
-})
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+        
+    })
+
+    request.write(jsonData);
+    request.end();
+});
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on 3000");
 });
 
-
+//api key
 // b0c876a1acad6d50b43a773159f4133e-us1
-
+// list id
 //defd742678
