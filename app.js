ba8a1b7e7df6053258fb4e499f031667227009d3
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-06-11 03:30:36 +0530

    deploying

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/failure", function(req,res){
     res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on 3000");
 });
 
